17b778aff6a7fb4707297a8765692c181a51fe04
Alec Newman <alec@newmanhome.org>
2016-04-26 00:06:44 -0400

    read_data seems to be fixed.  Also added pip requirements and setup stuff.

--- translate/translate.py
+++ translate/translate.py
@@ -40,6 +40,7 @@ import time
 import numpy as np
 from six.moves import xrange  # pylint: disable=redefined-builtin
 import tensorflow as tf
+from spacy import English
 
 from tensorflow.models.rnn.translate import data_utils
 from tensorflow.models.rnn.translate import seq2seq_model
@@ -73,42 +74,71 @@ FLAGS = tf.app.flags.FLAGS
 _buckets = [(5, 10), (10, 15), (20, 25), (40, 50)]
 
 
-def read_data(source_path, target_path, max_size=None):
+def read_data(source_path, max_size=None):
     """Read data from source and target files and put into buckets.
 
-  Args:
-    source_path: path to the files with token-ids for the source language.
-    target_path: path to the file with token-ids for the target language;
-      it must be aligned with the source file: n-th line contains the desired
-      output for n-th line from the source_path.
-    max_size: maximum number of lines to read, all other will be ignored;
-      if 0 or None, data files will be read completely (no limit).
+    Args:
+        source_path: path to the files with token-ids for the source language.
+        target_path: path to the file with token-ids for the target language;
+                     it must be aligned with the source file: n-th line contains
+                     the desired output for n-th line from the source_path.
+        max_size: maximum number of lines to read, all other will be ignored;
+                  if 0 or None, data files will be read completely (no limit).
 
-  Returns:
-    data_set: a list of length len(_buckets); data_set[n] contains a list of
-      (source, target) pairs read from the provided data files that fit
-      into the n-th bucket, i.e., such that len(source) < _buckets[n][0] and
-      len(target) < _buckets[n][1]; source and target are lists of token-ids.
-  """
+    Returns:
+        data_set: a list of length len(_buckets); data_set[n] contains a list of
+                  (source, target) pairs read from the provided data files that
+                  fit into the n-th bucket, i.e., such that
+                  len(source) < _buckets[n][0] and len(target) < _buckets[n][1];
+                  source and target are lists of token-ids.
+    """
+
     data_set = [[] for _ in _buckets]
-    with tf.gfile.GFile(source_path, mode="r") as source_file:
-        with tf.gfile.GFile(target_path, mode="r") as target_file:
-            source, target = source_file.readline(), target_file.readline()
-            counter = 0
-            while source and target and (not max_size or counter < max_size):
-                counter += 1
-                if counter % 100000 == 0:
-                    print("  reading data line %d" % counter)
-                    sys.stdout.flush()
-                source_ids = [int(x) for x in source.split()]
-                target_ids = [int(x) for x in target.split()]
-                target_ids.append(data_utils.EOS_ID)
-                for bucket_id, (source_size, target_size) in enumerate(_buckets):
-                    if len(source_ids) < source_size and len(target_ids) < target_size:
-                        data_set[bucket_id].append([source_ids, target_ids])
-                        break
-                source, target = source_file.readline(), target_file.readline()
+
+    words = {}
+
+    def word2int(word):
+        try:
+            return words[word]
+        except KeyError:
+            words[word] = len(words)
+            return words[word]
+
+    tokenizer = English(parser=False)
+
+    with open(source_path) as source:
+        reader = csv.reader(source)
+
+        for question, answer, label in reader:
+            source_ids = [word2int(token.lower_) for token in tokenizer(question + answer)]
+            target_ids = [word2int(token.lower_) for token in tokenizer(label)]
+
+            for bucket_id, (source_size, target_size) in enumerate(_buckets):
+                if len(source_ids) < source_size and len(target_ids) < target_size:
+                    data_set[bucket_id].append([source_ids, target_ids])
+                    break
+
     return data_set
+
+    #data_set = [[] for _ in _buckets]
+    #with tf.gfile.GFile(source_path, mode="r") as source_file:
+    #    with tf.gfile.GFile(target_path, mode="r") as target_file:
+    #        source, target = source_file.readline(), target_file.readline()
+    #        counter = 0
+    #        while source and target and (not max_size or counter < max_size):
+    #            counter += 1
+    #            if counter % 100000 == 0:
+    #                print("  reading data line %d" % counter)
+    #                sys.stdout.flush()
+    #            source_ids = [int(x) for x in source.split()]
+    #            target_ids = [int(x) for x in target.split()]
+    #            target_ids.append(data_utils.EOS_ID)
+    #            for bucket_id, (source_size, target_size) in enumerate(_buckets):
+    #                if len(source_ids) < source_size and len(target_ids) < target_size:
+    #                    data_set[bucket_id].append([source_ids, target_ids])
+    #                    break
+    #            source, target = source_file.readline(), target_file.readline()
+    #return data_set
 
 
 def create_model(session, forward_only):
@@ -132,8 +162,9 @@ def train():
     """Train a en->fr translation model using WMT data."""
     # Prepare WMT data.
     print("Preparing WMT data in %s" % FLAGS.data_dir)
-    en_train, fr_train, en_dev, fr_dev, _, _ = data_utils.prepare_wmt_data(
-        FLAGS.data_dir, FLAGS.en_vocab_size, FLAGS.fr_vocab_size)
+    train = '../data/train.csv'
+    test = '../data/test.csv'
+    valid = '../data/valid.csv'
 
     with tf.Session() as sess:
         # Create model.
@@ -143,8 +174,8 @@ def train():
         # Read data into buckets and compute their sizes.
         print("Reading development and training data (limit: %d)."
               % FLAGS.max_train_data_size)
-        dev_set = read_data(en_dev, fr_dev)
-        train_set = read_data(en_train, fr_train, FLAGS.max_train_data_size)
+        dev_set = read_data(train)
+        train_set = read_data(train, FLAGS.max_train_data_size)
         train_bucket_sizes = [len(train_set[b]) for b in xrange(len(_buckets))]
         train_total_size = float(sum(train_bucket_sizes))
 
@@ -274,6 +305,6 @@ def main(_):
     else:
         train()
 
-
 if __name__ == "__main__":
     tf.app.run()
+
