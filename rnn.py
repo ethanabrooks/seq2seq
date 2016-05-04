3ceb8ed4ee42dc0fc6e3c41af321b8b35c118528
Ethan <ethanabrooks@gmail.com>
2016-05-03 22:14:09 -0400

    try 9

--- rnn.py
+++ rnn.py
@@ -30,7 +30,7 @@ class Args:
         self.distinct_nums = 3
         self.vocabulary_size = self.distinct_nums * self.num_terms
         self.num_instances = self.distinct_nums ** self.num_terms / 2
-        self.num_cells = 12
+        self.num_cells = 9
         self.fold = 3
         self.batch_size = self.num_instances // self.fold
         self.multicell = True
